2668742702a06a079cce73a45666b9b3d5976a0f
choubung <choubung@sookmyung.ac.kr>
2024-10-05 20:53:43 +0900

    Build: runtimeOnly 'com.h2database:h2:1.4.197' 삭제

--- 이수연/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ 이수연/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -5,7 +5,6 @@ import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
