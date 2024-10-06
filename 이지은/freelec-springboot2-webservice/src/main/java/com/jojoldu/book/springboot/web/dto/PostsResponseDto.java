6e69c6738728148d033ddbaa769e9eb435d474b7
leejieun <mariewldms@naver.com>
2024-10-06 21:05:30 +0900

    Feat: Update, Response 기능 추가

---
+++ 이지은/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    //모든 생성자가 필요하지 않으므로 entity를 받아서 처리
+    public PostsResponseDto(Posts entity){
+        this.id=entity.getId();
+        this.title=entity.getTitle();
+        this.content=entity.getContent();
+        this.author=entity.getAuthor();
+    }
+}
