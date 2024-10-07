cd9cc96b3ee8b14de835edef45f866a056056f0d
hyeonseo <ohs4784@gmail.com>
2024-10-07 19:14:01 +0900

    @Transactional 추가

--- 오현서/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ 오현서/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -22,12 +22,20 @@ public class PostsService {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
-    public Long update(Long id, PostsResponseDto requestDto){
-        Posts posts = postsRepository.findBy(id).orElseThrow(()-> new
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(()-> new
                 IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
     }
 }
