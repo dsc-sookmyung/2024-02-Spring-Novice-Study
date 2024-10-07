95e266431c75c87f71e9746006ad1675a444d9d1
jwdebbie <zwn992@sookmyung.ac.kr>
2024-10-07 14:40:52 +0900

    Test : PostsRepositoryTest에BaseTimeEntity 테스트 메소드 추가 및 테스트완료

--- 이주원/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ 이주원/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -1,24 +1,27 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Getter // 롬복 어노테이션
-@NoArgsConstructor // 롬복 어노테이션
-@Entity  // JPA 어노테이션, 테이블과 링크될 클래스
-public class Posts {
-
-    @Id // 해당 테이블의 PK 필드를 나타냄
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 생성 규칙
+// Entity 클래스에서는 클래스의 인스턴스 값들의 변화를 구분하기 어렵기에 Setter 메소드를 절대 만들지 않음.
+@Getter             // 아래 2개는 lombok의 어노테이션
+@NoArgsConstructor
+@Entity   // JPA의 어노테이션
+// Posts가 BaseTimeEntity를 상속받도록 변경함.
+public class Posts extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 500, nullable = false) // 기본값 외 추가 변경 필요한 옵션 있을시 用
+    @Column(length = 500, nullable = false)
     private String title;
 
     @Column(columnDefinition = "TEXT", nullable = false)
@@ -26,17 +29,19 @@ public class Posts {
 
     private String author;
 
-    @Builder // 롬복 어노테이션
+    @Builder
     public Posts(String title, String content, String author) {
         this.title = title;
         this.content = content;
         this.author = author;
     }
+
     // update 함수 추가
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
     }
 }
+
 
 
