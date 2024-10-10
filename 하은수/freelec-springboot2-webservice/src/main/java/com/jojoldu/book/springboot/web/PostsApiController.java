21876eebe1f1b0de5181265b2b412b587809e710
ha9eun <ha9eunsoo@gmail.com>
2024-10-10 18:08:29 +0900

    Refactor: RequestMapping으로 코드 중복 제거

--- 하은수/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ 하은수/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -12,34 +12,31 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
 @RestController
 public class PostsApiController {
 
     private final PostsService postsService;
 
-    @PostMapping("/api/v1/posts")
+    @PostMapping
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
 
-    @PutMapping("/api/v1/posts/{id}")
+    @GetMapping
+    public List<PostsListResponseDto> getPostsList(){
+        return postsService.findAllDesc();
+    }
+
+    @PutMapping("/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
-    @GetMapping("/api/v1/posts/{id}")
-    public PostsResponseDto findById(@PathVariable Long id) {
-        return postsService.findById(id);
-    }
-
-    @DeleteMapping("/api/v1/posts/{id}")
+    @DeleteMapping("/{id}")
     public Long delete(@PathVariable Long id) {
         postsService.delete(id);
         return id;
     }
 
-    @GetMapping("/api/v1/posts")
-    public List<PostsListResponseDto> getPostsList(){
-        return postsService.findAllDesc();
-    }
 }
