64a9a7cf0e618a3e2d8f4650caef5a6d23d4778c
ruddmslee <dlruddms326@naver.com>
2024-10-12 02:10:26 +0900

    Feat: 전체 조회 화면 만들기

--- 이경은/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/dto/IndexController.java
+++ 이경은/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/dto/IndexController.java
@@ -1,16 +1,23 @@
 package com.jojoldu.book.springboot.web.dto;
 
+import com.jojoldu.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
+    private final PostsService postsService;
+
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
     }
 }
