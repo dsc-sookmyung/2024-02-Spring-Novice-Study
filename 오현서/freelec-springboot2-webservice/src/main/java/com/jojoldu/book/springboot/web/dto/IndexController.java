402979286c5b7807593e2697b89cfd9b3fd6e7f0
hyeonseo <ohs4784@gmail.com>
2024-10-14 02:24:03 +0900

    IndexController 파일 생성

---
+++ 오현서/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/dto/IndexController.java
@@ -0,0 +1,7 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class IndexController {
+}
