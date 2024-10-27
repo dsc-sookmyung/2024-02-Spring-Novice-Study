a73ed2f02e401e1b218e584757ead45ed7cae167
hyeonseo <ohs4784@gmail.com>
2024-10-27 15:39:31 +0900

    IndexController 수정

--- 오현서/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ 오현서/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,14 +1,18 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.h2.engine.Session;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,10 +20,18 @@ public class IndexController {
 
     //Ãß°¡
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user!=null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
