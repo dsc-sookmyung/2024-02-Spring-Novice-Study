578b1be8266fd38f41c2ca6534ee930d29b40c5b
leejieun <mariewldms@naver.com>
2024-10-27 21:50:03 +0900

    Feat: User entity, 스프링 시큐리티 설정

---
+++ 이지은/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.jojoldu.book.springboot.config.auth;
+
+import com.jojoldu.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigureAdapter{
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/","/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                //로그아웃 성공 시
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+
+    }
+
+}
