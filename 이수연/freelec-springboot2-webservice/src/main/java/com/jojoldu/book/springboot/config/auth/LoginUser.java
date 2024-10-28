73d8932abda3346f2d9bf79d733a87ce1021ae09
choubung <choubung@sookmyung.ac.kr>
2024-10-29 00:55:54 +0900

    Feat: LoginUser 파일 생성

---
+++ 이수연/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/config/auth/LoginUser.java
@@ -0,0 +1,9 @@
+package com.jojoldu.book.springboot.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public @interface LoginUser {
+}
