f16a078fecc072780c6a27df773ee34d2c95e438
ha9eun <ha9eunsoo@gmail.com>
2024-10-29 18:54:33 +0900

    Fix: 잘못된 어노테이션 수정

--- 하은수/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/config/JpaConfig.java
+++ 하은수/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/config/JpaConfig.java
@@ -1,10 +1,10 @@
 package com.jojoldu.book.springboot.config;
 
 
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@Configurable
+@Configuration
 @EnableJpaAuditing
 public class JpaConfig {
 }
