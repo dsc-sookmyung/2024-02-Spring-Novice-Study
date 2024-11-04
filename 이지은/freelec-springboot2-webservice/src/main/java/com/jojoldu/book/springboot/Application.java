cf7c761fb0625ac1cf4920e811695ceedb773339
leejieun <mariewldms@naver.com>
2024-11-04 21:47:18 +0900

    Docs: 5주차 스터디 완료

--- 이지은/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/Application.java
+++ 이지은/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/Application.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class Application {
-
+//5주차 스터디 완료
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
