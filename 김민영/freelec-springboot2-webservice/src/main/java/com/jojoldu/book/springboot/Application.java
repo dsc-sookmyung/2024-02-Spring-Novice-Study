018aff17ed16f522b6038e6990ef9f7658a1f121
minzero31 <77minyoung77@gmail.com>
2024-11-06 04:15:43 +0900

    Feat : AWS EC2 RDS 연동 완료

--- 김민영/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/Application.java
+++ 김민영/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/Application.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // @EnableJpaAuditing 제거
 // @EnableJpaAuditing  // JPA Auditing을 모두 활성화하도록 함.
+// aws ec2 rds 연동 인증을 위한 주석 추가
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
